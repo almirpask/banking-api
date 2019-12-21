0e236a67c2d5e7cd89856470dd5e8777ea08a1ab
Almir Santos <almirpask@hotmail.com>
2019-12-21 17:59:32 -0300

    create Account User

---
+++ priv/repo/migrations/20191221205804_create_users.exs
@@ -0,0 +1,12 @@
+defmodule BankingApi.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :" name", :string
+
+      timestamps()
+    end
+
+  end
+end
