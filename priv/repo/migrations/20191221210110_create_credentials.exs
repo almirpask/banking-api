bbbfdd5f9d87ca27dd6de52b6d94e7ccc4c70945
Almir Santos <almirpask@hotmail.com>
2019-12-21 18:09:14 -0300

    prevent null records and column names

--- priv/repo/migrations/20191221210110_create_credentials.exs
+++ priv/repo/migrations/20191221210110_create_credentials.exs
@@ -3,14 +3,14 @@ defmodule BankingApi.Repo.Migrations.CreateCredentials do
 
   def change do
     create table(:credentials) do
-      add :" email", :string
-      add :password_hash, :string
-      add :user_id, references(:users, on_delete: :nothing)
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
+      add :user_id, references(:users, on_delete: :nothing), null: false
 
       timestamps()
     end
 
-    create unique_index(:credentials, [: email])
+    create unique_index(:credentials, [:email])
     create index(:credentials, [:user_id])
   end
 end
