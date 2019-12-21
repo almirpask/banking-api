ddb0696eb66334d53427405fc8e1440cc0021b4e
Almir Santos <almirpask@hotmail.com>
2019-12-21 18:20:36 -0300

    create registration_changeset

--- lib/banking_api/accounts/user.ex
+++ lib/banking_api/accounts/user.ex
@@ -1,7 +1,7 @@
 defmodule BankingApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias Rumbl.Accounts.Credential
   schema "users" do
     field :" name", :string
 
@@ -11,7 +11,14 @@ defmodule BankingApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:" name"])
-    |> validate_required([:" name"])
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 1)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
   end
 end
